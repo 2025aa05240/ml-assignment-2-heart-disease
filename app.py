74fccb30369a02b83a60c9f2ccb6520a453c81df
2025aa05240 <2025aa05240@wilp.bits-pilani.ac.in>
2026-02-01 12:48:18 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="ML Assignment 2", layout="centered")
+
+st.title("Machine Learning Assignment 2")
+st.subheader("Heart Disease Classification")
+
+st.write("Streamlit app is set up successfully.")
